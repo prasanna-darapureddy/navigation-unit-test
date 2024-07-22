560728ceef373c411b63e2afbb70c89bca1d8749
Naga Prasanna Darapureddy <nagaprasanna18@gmail.com>
2024-07-22 17:20:41 +0530

    navigation unit test task

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SignUp from './components/register/signUp/SignUp';
+import Login from './components/register/login/Login';
+import Home from './components/register/home/Home';
+import SignUpFun from './components/register/signUp/SignUpFun';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<SignUp />} />
+        <Route path='/signup' element={<SignUpFun />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/Home' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
